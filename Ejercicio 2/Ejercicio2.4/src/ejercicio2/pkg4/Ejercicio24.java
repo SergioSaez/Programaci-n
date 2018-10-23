928dd41ac1c63292457670f422c355df749b3022
Sergio Saez <sergio.saez@ikasle.egibide.org>
2018-10-23 15:22:48 +0100

    ej 2.4

--- Ejercicio 2/Ejercicio2.4/src/ejercicio2/pkg4/Ejercicio24.java
+++ Ejercicio 2/Ejercicio2.4/src/ejercicio2/pkg4/Ejercicio24.java
@@ -1,9 +1,13 @@
 package ejercicio2.pkg4;
 
+import java.util.Calendar;
+
 public class Ejercicio24 {
 
     public static void main(String[] args) {
-   
+        Calendar sameDate = Calendar.getInstance();
+        System.out.println(sameDate);
+        
     }
 }
 
