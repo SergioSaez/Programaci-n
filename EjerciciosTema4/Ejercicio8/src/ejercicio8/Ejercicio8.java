efa1a90f42997d5b92ef54d5c6508621c36dfbf5
Sergio Saez <sergio.saez@ikasle.egibide.org>
2018-11-23 14:04:41 +0100

    23/11/2018

--- EjerciciosTema4/Ejercicio8/src/ejercicio8/Ejercicio8.java
+++ EjerciciosTema4/Ejercicio8/src/ejercicio8/Ejercicio8.java
@@ -1,21 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio8;
 
-/**
- *
- * @author Sergio
- */
-public class Ejercicio8 {
+import javax.swing.JOptionPane;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Ejercicio8 {
     
+    public static void main(String[] args) {
+        int [][] matriz = new int [10] [10];
+        int contador =0;
+        String continuar = null;
+        do{
+        try{
+            String val1 = JOptionPane.showInputDialog ("Introduce el numero de la fila (entre 0 y 10)");
+            int v1=Integer.parseInt(val1);
+            if(v1 > 10 || v1 < 0){
+                JOptionPane.showMessageDialog(null, "El numero introducido es menor que 0 o mayor que 10");
+                System.exit(0);
+            }
+            else{}
+            String val2 = JOptionPane.showInputDialog ("Introduce el numero de la columna (entre 0 y 10)");
+            int v2=Integer.parseInt(val2);
+            if(v2 > 10 || v1 < 0){
+                JOptionPane.showMessageDialog(null, "El numero introducido es menor que 0 o mayor que 10");
+                System.exit(0);
+            }
+            else{}
+            String val3 = JOptionPane.showInputDialog("Introduce el valor que quieres que este en ese lugar");
+            int v3=Integer.parseInt(val3);
+            matriz [v1][v2] = v3;
+            contador ++;
+        }
+        catch(Exception ex){ 
+            JOptionPane.showMessageDialog(null, "Error");
+        }
+        continuar = JOptionPane.showInputDialog("Quieres continuar?");
+        }while(contador <= 5 || continuar== "no");
+    }  
 }
